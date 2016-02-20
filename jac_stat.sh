b0576315011aa75f6988301fd4a6f3b297e444c4
mackie90125 <mackie90125@gmail.com>
2016-02-19 17:55:13 -0700

    change bonus question

--- jac_stat.sh
+++ jac_stat.sh
@@ -2,38 +2,43 @@
 
 ## MOLB 7621 Problem Set 2
 ## Extra Credit Question:
-## Using bedtools jaccard, what transcription factor binding sites in encode.tfbs.chr22.bed.gz 
-## are most similar to CTCF binding sites? 
+## Using bedtools jaccard, what two transcription factor binding sites 
+## in encode.tfbs.chr22.bed.gz are most similar? 
 
 
 # Files to be used
 tfbs='/home/ubuntu/MOLB-7621/data-sets/bed/encode.tfbs.chr22.bed.gz'
 
 # Create list of all transcription factors
-zcat encode.tfbs.chr22.bed.gz | cut -f4 | sort | uniq > all_tf.txt
-
-# Create bed file for CTCF binding sites
-zcat $tfbs | awk 'BEGIN {OFS="\t"} ($4 == "CTCF")' > CTCF.bed
+zcat $tfbs | cut -f4 | sort | uniq > all_tf.txt
 
 # Create directory for all BED files that will be created
 mkdir bed_files
 
-
-# Jaccard statistic for each transcription factor compared to CTCF
-(
+# Create BED file for each transcription factor
 for line in `cat all_tf.txt`; do
-    
+
     zcat $tfbs | awk -v var="$line" 'BEGIN {OFS="\t"} ($4 == var)' > bed_files/$line.bed
 
-    jaccard=$(bedtools jaccard -a CTCF.bed -b bed_files/$line.bed | tail -n1 | cut -f3)
+done
 
-    echo -e ''$line'\tCTCF\t'$jaccard''
+# Jaccard statistic for each pair of transcription factors
+(
+for line1 in `cat all_tf.txt`; do
+    for line2 in `cat all_tf.txt`; do    
+
+        if [ $line1 == $line2 ]; then
+            continue
+        fi
+        
+        jaccard=$(bedtools jaccard -a bed_files/$line1.bed -b bed_files/$line2.bed | tail -n1 | cut -f3)
+
+        echo -e ''$line1'\t'$line2'\t'$jaccard''
+
+    done
 
 done
-) | sort -k3 -nr > all_jac.tsv
+) > all_jac.tsv
 
-
-# Answer: RAD21
-# This output makes sense because CTCF gives a perfect match to CTCF (obviously),
-# and CTCF and RAD21 are both involved in chromatin regulation at some level,
-# so it makes sense that their binding sites would be similar
+# Print answer
+sort -k3 -nr all_jac.tsv | awk '$3 !~ /e/' | head -n1
